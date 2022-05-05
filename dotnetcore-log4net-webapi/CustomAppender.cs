4e2ab94de9a9f5f710e22352468861621ee26220
Derek <lee.derek@bcg.com>
2022-05-05 06:52:13 -0500

    added gitignore. custom appenders. Fix to connectionstring

---
+++ dotnetcore-log4net-webapi/CustomAppender.cs
@@ -0,0 +1,54 @@
+﻿using log4net.Appender;
+using log4net.Core;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace dotnetcore_log4net_webapi
+{
+	public class CustomAppender : AdoNetAppender
+	{
+		protected override void SendBuffer(IDbTransaction dbTran, LoggingEvent[] events)
+		{
+			if (CommandText != null && CommandText.Trim() != "")
+			{
+				using (IDbCommand dbCmd = Connection.CreateCommand())
+				{
+					// Set the command string
+					dbCmd.CommandText = CommandText;
+
+					// Set the command type
+					dbCmd.CommandType = CommandType;
+					// Send buffer using the prepared command object
+					if (dbTran != null)
+					{
+						dbCmd.Transaction = dbTran;
+					}
+
+					// clear parameters that have been set
+					dbCmd.Parameters.Clear();
+
+					// Set the parameter values
+					foreach (AdoNetAppenderParameter param in m_parameters)
+						param.Prepare(dbCmd);
+
+					// prepare the command, which is significantly faster
+					dbCmd.Prepare();
+
+					// run for all events
+					foreach (LoggingEvent e in events)
+					{
+						// Set the parameter values
+						foreach (AdoNetAppenderParameter param in m_parameters)
+							param.FormatValue(dbCmd, e);
+
+						// Execute the query
+						dbCmd.ExecuteNonQuery();
+					}
+				}
+			}
+		}
+	}
+}
